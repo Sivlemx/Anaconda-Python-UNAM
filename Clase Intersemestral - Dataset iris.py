cbf2eb6d5e115423aa03acf5387b14f2365fff72
Sivlemx <sivlemx@gmail.com>
2017-06-13 22:33:17 -0500

    Actualización

---
+++ Clase Intersemestral - Dataset iris.py
@@ -0,0 +1,143 @@
+#### Base de datos "iris" ############################################################
+import pandas as pd
+import matplotlib.pyplot as plt
+
+iris_csv = 'iris.csv'
+print(iris_csv)
+print(type(iris_csv))
+
+df_csv = pd.read_csv(iris_csv, sep=',')
+print(df_csv)
+print(type(df_csv))
+
+iris_xlsx = 'iris.xlsx'
+print(iris_xlsx)
+print(type(iris_xlsx))
+
+df_xlsx = pd.ExcelFile(iris_xlsx)
+print(df_xlsx.sheet_names)
+df1 = df_xlsx.parse('iris') # --> 1ra opción
+df2 = df_xlsx.parse(0) # --> 2da opción
+print(df1)
+print(type(df1))
+print(df2)
+print(type(df2))
+########################################################################################
+#### Indices y Columnas
+print(df1.shape)
+
+print(df1.columns)
+print(type(df1.columns))
+
+print(df1.index)
+print(type(df1.index))
+########################################################################################
+#### Dividiendo filas y columnas
+## comando "iloc" y "loc"
+primeras_filas = df1.iloc[:5, :]
+print(primeras_filas)
+
+ultimas_filas = df1.iloc[-5:, :]
+print(ultimas_filas)
+
+iloc_columnas = df1.iloc[:, :3]
+print(iloc_columnas)
+
+loc_columnas = df1.loc[:, :'petal_length']
+print(loc_columnas)
+########################################################################################
+#### comando "head()" y "tail()"
+print(df1.head())
+print(df1.head(10))
+print(df1.head(20))
+
+print(df1.tail())
+print(df1.tail(10))
+print(df1.tail(20))
+#########################################################################################
+#### comando "info()"
+print(df1.info())
+########################################################################################
+#### comando "unique()"
+print(df1['name'])
+
+print(df1['name'].unique())
+#########################################################################################
+#### Line Plot
+df1.plot(x='sepal_length', y='sepal_width')
+plt.show()
+
+#### Scatter Plot
+df1.plot(x='sepal_length', y='sepal_width', kind='scatter')
+plt.xlabel('Ancho de Sepalo (cm)')
+plt.ylabel('Largo de Sepalo (cm)')
+plt.show()
+
+#### Boxplot
+df1.plot(y='sepal_length', kind='box')
+plt.ylabel('Largo de Sepalo (cm)')
+plt.show()
+
+#### Histograma
+df1.plot(y='sepal_length', kind='hist')
+plt.xlabel('Ancho de Sepalo (cm)')
+plt.show()
+
+#### Histograma
+df1.plot(y='sepal_length', kind='hist', bins=30, range=(4,8), normed=True)
+plt.xlabel('Ancho de Sepalo (cm)')
+plt.show()
+
+
+#### Estadística Descriptiva
+
+## Medidas de Tendencia Central = media, mediana, moda
+df1[df1['name'] == 'versicolor']['sepal_length'].mean() # Media
+df1[df1['name'] == 'versicolor']['sepal_length'].median() # Mediana
+df1[df1['name'] == 'versicolor']['sepal_length'].mode() # Moda
+
+## Medidas de Dispersión = Rango, Desviación Estándar, Varianza
+df1[df1['name'] == 'versicolor']['sepal_length'].max() # Máximo
+df1[df1['name'] == 'versicolor']['sepal_length'].min() # Mínimo
+df1[df1['name'] == 'versicolor']['sepal_length'].std() # Desviación Estándar
+df1[df1['name'] == 'versicolor']['sepal_length'].var() # Varianza
+
+df1[df1['name'] == 'versicolor']['sepal_length'].sem() # Error Estándar
+
+
+df1[df1['name'] == 'versicolor'].describe()
+
+df1.describe()
+
+
+
+########################################################################################
+#### comando "groupby"
+groupby_name = df1.groupby('name')
+
+for name, value in groupby_name['sepal_length']:
+    print((name, value.mean()))
+
+groupby_name.mean()
+groupby_name.median()
+groupby_name.mode() # Groupby no da la moda
+groupby_name.std()
+groupby_name.sem()
+
+groupby_name.boxplot()
+df1[df1['name'] == 'setosa'].boxplot()
+df1[df1['name'] == 'versicolor'].boxplot()
+df1[df1['name'] == 'virginica'].boxplot()
+
+### Plotear datos
+import matplotlib.pyplot as plt
+
+pd.plotting.scatter_matrix(df1[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+df1[df1['name'] == 'versicolor']['sepal_length'].plot()
+df1[df1['name'] == 'versicolor']['sepal_length']
+df1[df1['name'] == 'versicolor']['sepal_length']
+df1[df1['name'] == 'versicolor']['sepal_length']
+df1[df1['name'] == 'versicolor']['sepal_length']
+
+df1[df1['name'] == 'versicolor'].plot.scatter('sepal_length', 'petal_length')
+
--- Clase Intersemestral - pandas.py
+++ Clase Intersemestral - pandas.py
@@ -87,8 +87,10 @@ print(
 ##########################################################################################
 
 import pandas as pd
+# http://pandas.pydata.org/pandas-docs/stable/index.html
 
 import numpy as np
+# https://docs.scipy.org/doc/numpy/user/quickstart.html
 
 #### DataFrame desde cero
 ## Los diccionarios deben contener el mismo número de "values"
@@ -115,9 +117,9 @@ print(clase)
 peso = [67, 56, 69, 55, 65, 51, 47, 90, 52, 56, 82, 104, 89, 65, 60]
 estatura = [1.74, 1.60, 1.71, 1.60, 1.70, 1.60, 1.53, 1.73, 1.60, 1.60, 1.76, 1.80, 1.78, 1.72, 1.52]
 
-
+# Cambiar Macho=Hombre y Hembra=Mujer
 clase = {'Semestre':['Sexto', 'Sexto', 'Sexto', 'Sexto', 'Cuarto', 'Egresada', 'Segundo', 'Cuarto', 'Sexto', 'Sexto', 'Titulado', 'Doctorante', 'Egresada', 'Sexto', 'Egresada'],
-         'Sexo':['Mujer', 'Mujer', 'Hombre', 'Mujer', 'Hombre', 'Mujer', 'Mujer', 'Hombre', 'Mujer', 'Mujer', 'Hombre', 'Macho', 'Mujer', 'Hombre', 'Hembra'],
+         'Sexo':['Mujer', 'Mujer', 'Hombre', 'Mujer', 'Hombre', 'Mujer', 'Mujer', 'Hombre', 'Mujer', 'Mujer', 'Hombre', 'Hombre', 'Mujer', 'Hombre', 'Mujer'],
          'Edad':[23, 20, 21, 22, 25, 23, 19, 20, 21, 20, 25, 34, 22, 21, 24],
          'Estatura':[1.74, 1.60, 1.71, 1.60, 1.70, 1.60, 1.53, 1.73, 1.60, 1.60, 1.76, 1.80, 1.60, 1.72, 1.52],
          'Nombre':['Gisela', 'Valeria', 'Alfredo', 'Gabriela', 'Pako', 'Samantha', 'Valeria', 'Mario', 'Elizabeth', 'Sam', 'Christian', 'Javier', 'Leslie', 'Rodrigo', 'Yuna']}
@@ -169,19 +171,19 @@ type(clase[['Estatura']])
 type(clase[['Deporte']])
 type(clase[['Nombre']])
 
-clase[['Peso', 'Categoría_IMC']]
+clase[['Deporte', 'Nombre']]
 
 clase[1:3]
 
 ## Método "*.loc"
 # Filas
-clase.loc['Pe']
-clase.loc[['Pe']]
-clase.loc[['Pe', 'Fu', 'Sut']]
+clase.loc['GC']
+clase.loc[['GC']]
+clase.loc[['LZ', 'EN', 'GB']]
 # Filas y Columnas
-clase.loc[[1, 2, 3], ['Peso', 'Categoría_IMC']]
+clase.loc[['PT', 'CD', 'ID'], ['Deporte', 'Nombre']]
 #Todas las Filas
-clase.loc[:, ['Peso', 'Categoría_IMC']]
+clase.loc[:, ['Deporte', 'Nombre']]
 
 ## Método "*.iloc"
 # Filas
@@ -189,9 +191,18 @@ clase.iloc[1]
 clase.iloc[[1]]
 clase.iloc[[1, 2, 3]]
 # Filas y Columnas
-clase.iloc[[1, 2, 3], [6, 9]]
+clase.iloc[[1, 2, 3], [6, 7]]
 #Todas las Filas
-clase.iloc[:, [6, 9]]
+clase.iloc[:, [6, 7]]
+
+## Ejercicio
+# Utilizar = *.loc[Edad, Estatura, Peso] de JV, SA, CD
+# En 2 tipos = Series de Tiempo y DataFrame
+
+# Utilizar = *.iloc[Edad, Estatura, Peso] de JV, SA, CD
+# En 2 tipos = Series de Tiempo y DataFrame
+
+
 
 import numpy as np
 
@@ -225,42 +236,7 @@ clase = obeso.assign(Categoría_IMC = 'Obeso').append(df2)
 clase.Categoría_IMC = clase.Categoría_IMC.astype('category')
 
 ## Ordenar por Edad
-#clase = clase.sort_values('Edad')
-
-## Exportar a CSV
-clase.to_csv('Clase_Anaconda.csv', sep=',')
-
-## Exportar a Excel
-clase.to_excel('Clase_Anaconda.xls')
-clase.to_excel('Clase_Anaconda.xlsx')
-
-
-## Importar archivo CSV
-clase = pd.read_csv('Clase_Anaconda.csv', sep=',')
-
-## Bucle For
-clase = pd.read_csv('Clase_Anaconda.csv', sep=',', index_col=0)
-
-for val in clase:
-    print(val)
-
-for sujeto, valores in clase.iterrows():
-    print(sujeto)
-    print(valores)
-
-for sujeto, valores in clase.iterrows():
-    print(sujeto + ': ' + valores['Nombre'])
-
-## Agregar columna con For
-## Y función "*.apply()"
-for sujeto, valores in clase.iterrows():
-    clase.loc[sujeto, 'No._Letras'] = len(valores['Nombre'])
-print(clase)
-
-clase['No. Letras'] = clase['Nombre'].apply(len)
-print(clase)
-
-clase = clase.drop('No._Letras', axis=1)
+clase = clase.sort_values('Edad')
 
 ## Índices y Columnas
 type(clase)
@@ -281,9 +257,74 @@ clase.tail(2)
 
 clase.info()
 
-### Función Groupby
+
+## Exportar a CSV
+clase.to_csv('Clase_Anaconda.csv', sep=',')
+
+## Exportar a TXT
+clase.to_csv('Clase_Anaconda.txt', sep=',')
+
+## Exportar a Excel
+clase.to_excel('Clase_Anaconda.xls')
+clase.to_excel('Clase_Anaconda.xlsx')
+
+
+## Importar archivo CSV
+clase = pd.read_csv('Clase_Anaconda.csv', sep=',')
+
+clase = pd.read_csv('Clase_Anaconda.csv', sep=',', index_col=0)
+
+## Bucle For
+
+for val in clase:
+    print(val)
+
+for sujeto, valores in clase.iterrows():
+    print(sujeto)
+    print(valores)
+
+for sujeto, valores in clase.iterrows():
+    print(sujeto + ': ' + valores['Nombre'])
+
+## Agregar columna con For
+## Y función "*.apply()"
+for sujeto, valores in clase.iterrows():
+    clase.loc[sujeto, 'No._Letras'] = len(valores['Nombre'])
+print(clase)
+
+##
+clase['# Letras en el Nombre'] = clase['Nombre'].apply(len)
+print(clase)
+
+clase = clase.drop('No._Letras', axis=1)
+
+## Ejercicio
+# 1.- Fijate en el ejemplo de arriba
+# 2.- Crea una columna llamada "Letras UNAM" con valores del número de letras
+# 3.- Avisa que ya lo hiciste
+# 4.- Esa columna que creaste, borrala
+
+
+
+
+
+
+
+
+
+
+
+
+clase['Letras UNAM'] = clase['Universidad'].apply(len)
+clase = clase.drop('# Letras UNAM', axis=1)
+
+################################################################
+
 clase['Sexo']
-clase.groupby('Sexo').mean()
+clase[['Sexo']]
+
+clase['Sexo'].unique()
+clase[['Sexo']].nunique()
 
 #### Estadística Descriptiva
 print("""
@@ -297,28 +338,48 @@ print("""
 clase[clase['Sexo'] == 'Mujer']['Estatura'].mean() # Media
 clase[clase['Sexo'] == 'Mujer']['Estatura'].median() # Mediana
 clase[clase['Sexo'] == 'Mujer']['Estatura'].mode() # Moda
-clase.groupby('Sexo').mean()
-clase.groupby('Sexo').median()
-clase.groupby('Sexo').mode()
 
 ## Medidas de Dispersión = Rango, Desviación Estándar, Varianza
 clase[clase['Sexo'] == 'Mujer']['Estatura'].max() # Máximo
 clase[clase['Sexo'] == 'Mujer']['Estatura'].min() # Mínimo
 clase[clase['Sexo'] == 'Mujer']['Estatura'].std() # Desviación Estándar
 clase[clase['Sexo'] == 'Mujer']['Estatura'].var() # Varianza
+
+clase[clase['Sexo'] == 'Mujer']['Estatura'].sem() # Error Estándar
+
+clase[clase['Sexo'] == 'Mujer'][['Estatura', 'Peso']].describe() # Datos Descriptivos 
+
+#Correlación (method='pearson')
+clase[clase['Sexo'] == 'Mujer'].corr()
+
+## Ejercicio
+# 1.- Obtener la media de la Edad y el IMC de solo los hombres 
+# 2.- Obtener la desviación estandar de la Edad y el IMC de solo los hombres
+# 3.- Obtener el mínimo de la Edad y el IMC de solo los hombres
+# 4.- Obtener el máximo de la Edad y el IMC de solo los hombres
+# 2.- Obtener la correlación de la Edad y el IMC de solo los hombres
+
+
+### Función Groupby
+clase['Sexo'].unique()
+
+clase.groupby('Sexo').mean()
+clase.groupby('Sexo').median()
+clase.groupby('Sexo').mode()
+
 clase.groupby('Sexo').max()
 clase.groupby('Sexo').min()
 clase.groupby('Sexo').std()
 clase.groupby('Sexo').var()
 
-clase[clase['Sexo'] == 'Mujer']['Estatura'].sem() # Error Estándar
 clase.groupby('Sexo').sem()
 
-clase[clase['Sexo'] == 'Mujer'].describe()
 clase.groupby('Sexo').describe()
 
-clase.describe()
+clase.groupby('Sexo').corr()
 
+clase.describe()
+clase.corr()
 #############################################################
 #### Análisis Cualitativo
 # Factores e individuales
@@ -336,17 +397,38 @@ hombres = clase[clase['Sexo'] == 'Hombre']
 
 mujeres.describe()
 hombres.describe()
-
+############################################################################################
 #### Graficar lo que se tenga que graficar
 import matplotlib.pyplot as plt
+# https://matplotlib.org/index.html
 
 edades = clase['Edad'].values
+
+plt.hist(edades)
+plt.hist(edades, 5)
+plt.hist(edades, 10)
+
 plt.plot(edades)
 plt.show()
 
-edades1 = clase['Edad']
-edades1.plot()
+x = clase['Peso'].values
+y = clase['Estatura'].values
+plt.scatter(x, y)
 plt.show()
+##########################################################################################
+## Graficando con la propia librería de pandas
+edades1 = clase['Edad']
+
+edades1.plot.hist()
+edades1.plot.hist(alpha=0.5)
+
+edades1.plot()
+
+clase.plot.scatter(x='Peso', y='Estatura')
+
+ax = clase[clase['Sexo'] == 'Mujer'].plot.scatter(x='Peso', y='Estatura', color='Red', label='Mujeres')
+clase[clase['Sexo'] == 'Hombre'].plot.scatter(x='Peso', y='Estatura', color='Blue', label='Hombres', ax=ax)
+
 
 clase.loc[:, ['IMC', 'Peso']].plot()
 
@@ -355,9 +437,21 @@ pd.plotting.scatter_matrix(clase[['Peso', 'Estatura', 'IMC']])
 clase.groupby('Sexo').boxplot()
 
 clase.groupby('Sexo')['Peso'].describe()
+######################################################################################
+import seaborn as sns
+# http://seaborn.pydata.org/index.html
+sns.pairplot(clase, vars=['Peso', 'Estatura', 'IMC'], kind='reg')
+
+sns.pairplot(clase, vars=['Peso', 'Estatura', 'IMC'], kind='reg', hue='Sexo')
+
+sns.lmplot(y='Peso', x='Estatura', data=clase)
+
+sns.stripplot(x='Peso', y='Estatura', hue='Sexo', data=clase)
+
+sns.stripplot(x='Categoría_IMC', y='IMC', hue='Sexo', data=clase)
 
 ###########################################################################################
-### Esatadística Inferencial
+### Estadística Inferencial
 import scipy.stats as sps
 # https://docs.scipy.org/doc/scipy/reference/stats.html#module-scipy.stats
 
@@ -365,183 +459,6 @@ peso_mujer = clase[clase['Sexo'] == 'Mujer']['Peso']
 peso_hombre = clase[clase['Sexo'] == 'Hombre']['Peso']
 sps.ttest_ind(peso_mujer, peso_hombre)
 
-
-#### Base de datos "iris" ############################################################
-from urllib.request import urlretrieve
-
-url = 'https://raw.githubusercontent.com/Sivlemx/Anaconda-Python-UNAM/master/iris.csv'
-urlretrieve(url, 'iris.csv')
-iris_csv = 'iris.csv'
-print(iris_csv)
-print(type(iris_csv))
-
-df_csv = pd.read_csv(iris_csv, sep=',')
-print(df_csv)
-print(type(df_csv))
-
-url = 'https://github.com/Sivlemx/Anaconda-Python-UNAM/blob/master/iris.xlsx'
-urlretrieve(url, 'iris.xlsx')
-iris_xlsx = 'iris.xlsx'
-print(iris_xlsx)
-print(type(iris_xlsx))
-
-df_xlsx = pd.ExcelFile(iris_xlsx)
-print(df_xlsx.sheet_names)
-df1 = df_xlsx.parse('iris') # --> 1ra opción
-df2 = df_xlsx.parse(0) # --> 2da opción
-print(df1)
-print(type(df1))
-print(df2)
-print(type(df2))
-########################################################################################
-#### Indices y Columnas
-print(df1.shape)
-
-print(df1.columns)
-print(type(df1.columns))
-
-print(df1.index)
-print(type(df1.index))
-########################################################################################
-#### Dividiendo filas y columnas
-## comando "iloc" y "loc"
-primeras_filas = df1.iloc[:5, :]
-print(primeras_filas)
-
-ultimas_filas = df1.iloc[-5:, :]
-print(ultimas_filas)
-
-iloc_columnas = df1.iloc[:, :3]
-print(iloc_columnas)
-
-loc_columnas = df1.loc[:, :'petal_length']
-print(loc_columnas)
-########################################################################################
-#### comando "head()" y "tail()"
-print(df1.head())
-print(df1.head(10))
-print(df1.head(20))
-
-print(df1.tail())
-print(df1.tail(10))
-print(df1.tail(20))
-#########################################################################################
-#### comando "info()"
-print(df1.info())
-########################################################################################
-#### comando "unique()"
-print(df1['name'])
-
-print(df1['name'].unique())
-#########################################################################################
-#### Line Plot
-df1.plot(x='sepal_length', y='sepal_width')
-plt.show()
-
-#### Scatter Plot
-df1.plot(x='sepal_length', y='sepal_width', kind='scatter')
-plt.xlabel('Ancho de Sepalo (cm)')
-plt.ylabel('Largo de Sepalo (cm)')
-plt.show()
-
-#### Boxplot
-df1.plot(y='sepal_length', kind='box')
-plt.ylabel('Largo de Sepalo (cm)')
-plt.show()
-
-#### Histograma
-df1.plot(y='sepal_length', kind='hist')
-plt.xlabel('Ancho de Sepalo (cm)')
-plt.show()
-
-#### Histograma
-df1.plot(y='sepal_length', kind='hist', bins=30, range=(4,8), normed=True)
-plt.xlabel('Ancho de Sepalo (cm)')
-plt.show()
-
-
-
-
-
-
-
-#### Estadística Descriptiva
-print("""
-      Estadística Descriptiva:
-          
-          “Es el estudio que incluye la obtención, organización, 
-          presentación y descripción de información numérica”. 
-""")
-
-## Medidas de Tendencia Central = media, mediana, moda
-df1[df1['name'] == 'versicolor']['sepal_length'].mean() # Media
-df1[df1['name'] == 'versicolor']['sepal_length'].median() # Mediana
-df1[df1['name'] == 'versicolor']['sepal_length'].mode() # Moda
-
-## Medidas de Dispersión = Rango, Desviación Estándar, Varianza
-df1[df1['name'] == 'versicolor']['sepal_length'].max() # Máximo
-df1[df1['name'] == 'versicolor']['sepal_length'].min() # Mínimo
-df1[df1['name'] == 'versicolor']['sepal_length'].std() # Desviación Estándar
-df1[df1['name'] == 'versicolor']['sepal_length'].var() # Varianza
-
-df1[df1['name'] == 'versicolor']['sepal_length'].sem() # Error Estándar
-
-
-
-
-
-
-
-df1[df1['name'] == 'versicolor'].describe()
-
-df1.describe()
-
-
-
-
-## Estadística Parámetrica
-## Estadística No Parámetrica
-## Correlación
-## Regresión Lineal
-
-
-
-print("""
-      Esatdística Inferencial:
-          “La inferencia estadística es una técnica mediante la cual se 
-          obtienen generalizaciones o se toman decisiones en base a una 
-          información parcial o completa obtenida mediante 
-          técnicas descriptivas”. 
-""")
-#########################################################################################
-#### comando "groupby"
-groupby_name = df1.groupby('name')
-
-for name, value in groupby_name['sepal_length']:
-    print((name, value.mean()))
-
-groupby_name.mean()
-groupby_name.median()
-groupby_name.mode() # Groupby no da la moda
-groupby_name.std()
-groupby_name.sem()
-
-groupby_name.boxplot()
-df1[df1['name'] == 'setosa'].boxplot()
-df1[df1['name'] == 'versicolor'].boxplot()
-df1[df1['name'] == 'virginica'].boxplot()
-
-### Plotear datos
-import matplotlib.pyplot as plt
-
-pd.plotting.scatter_matrix(df1[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
-df1[df1['name'] == 'versicolor']['sepal_length'].plot()
-df1[df1['name'] == 'versicolor']['sepal_length']
-df1[df1['name'] == 'versicolor']['sepal_length']
-df1[df1['name'] == 'versicolor']['sepal_length']
-df1[df1['name'] == 'versicolor']['sepal_length']
-
-df1[df1['name'] == 'versicolor'].plot.scatter('sepal_length', 'petal_length')
 
 
 
